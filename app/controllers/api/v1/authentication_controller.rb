8ef97bedf8de03f33db082bb55e22a5ad4b485a6
DikuCoding <dikeshkhagi1@gmail.com>
2024-12-24 17:31:10 +0545

    add authentication_controller

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,41 @@
+module api
+  module v1
+    class AuthenticationController < ApplicationController
+      require 'jwt'
+      # POST /api/v1/signup
+      def signup
+        user = User.new(user_params)
+        if user.save
+          token = encode_token(user.id)
+          render json: {user: user, token: token}, status: :created
+        else
+          render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+      end
+
+      #POST /api/v1/login
+      def login
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+          token = encode_token(user.id)
+          render json: { user: user, token: token }, status: :ok
+        else
+          render json: {errors: ['Invalid email or password']}, status: :unauthorized
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:email, :password, :password_confirmation)
+      end
+
+      def encode_token(user_id)
+        payload = { user_id: user_id, exp: 24.hours.from_now.to_i }
+        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      end
+
+    end
+  end
+end
+
